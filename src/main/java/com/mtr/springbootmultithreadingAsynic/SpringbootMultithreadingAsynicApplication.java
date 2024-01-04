8c19c03af827c88ab78c0c878c3592bb8c2f6bc4
muthahar20@gmail.com <muthahar20@gmail.com>
2024-01-04 10:08:05 -0500

    initial commit

---
+++ src/main/java/com/mtr/springbootmultithreadingAsynic/SpringbootMultithreadingAsynicApplication.java
@@ -0,0 +1,13 @@
+package com.mtr.springbootmultithreadingAsynic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultithreadingAsynicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMultithreadingAsynicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtr/springbootmultithreadingAsynic/SpringbootMultithreadingAsynicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtr.springbootmultithreadingAsynic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMultithreadingAsynicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
